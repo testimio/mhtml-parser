3a3cc5e398f1fb782f8bcc369b7c45bc1f1d22a9
Benjamin Gruenbaum <benjamingr@gmail.com>
2018-12-17 13:28:01 +0200

    new public demos for open sourcing

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@
 module.exports.Parser = require('./parser');
 // mhtml reader
 module.exports.Processor = require('./processor');
+
+if (require.main === module) {
+ // serverm ode
+ module.exports.Processor.serve(process.env.PORT || 8080);  
+}
