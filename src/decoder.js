4e6b5c810471625d4ecd605788f6ea41c3d4d0cb
Gilad Novik <gilad@novik.ca>
2018-12-18 23:23:05 +0200

    loop optimization

--- src/decoder.js
+++ src/decoder.js
@@ -16,74 +16,48 @@ module.exports = (encoding, body) => {
 const EQUALS = '='.charCodeAt(0);
 const CR = '\r'.charCodeAt(0);
 const LF = '\n'.charCodeAt(0);
-const ZERO = '0'.charCodeAt(0);
-const NINE = '9'.charCodeAt(0);
-const A = 'A'.charCodeAt(0);
-const F = 'F'.charCodeAt(0);
 const D = 'D'.charCodeAt(0);
 const THREE = '3'.charCodeAt(0);
 
-const isAsciiNum = code => code >= ZERO && code <= NINE;
-const isHexDigit = code => code >= A && code <= F;
+const hex = Buffer.alloc(256, -1);
+for (let i = 0; i <= 9; i++) hex['0'.charCodeAt(0) + i] = i;
+for (let i = 0xa; i <= 0xf; i++) hex['A'.charCodeAt(0) + i - 0xa] = i;
 
 function convertQuotedPrintable(body) {
   const len = body.length;
   const decoded = Buffer.alloc(len); // at most this big
-  const runTo = len - 3;
   let j = 0;
-  for (let i = 0; i < runTo; i++) {
-    if (body[i] !== EQUALS) {
-      decoded[j++] = body[i];
-      continue;
-    }
+  for (let i = 0; i < len; i++) {
+    while (i < len && (decoded[j++] = body[i++]) !== EQUALS);
+    if (i >= len) break;
     // We are dealing with a '=xx' sequence.
-    const upper = body[++i];
+    const upper = body[i];
     const lower = body[++i];
 
     // fast path for =3D
     if (upper === THREE && lower === D) {
-      decoded[j++] = EQUALS;
       continue;
     }
-    let upperTranslated = 1000;
-    let lowerTranslated = 1000;
     if (upper === CR && lower === LF) {
+      j--;
       continue;
     }
-    if (upper === LF) { // windows chrome does invalid encoding with \n and not \r\n
+    if (upper === LF) {
+      // windows chrome does invalid encoding with \n and not \r\n
       i--;
+      j--;
       continue;
     }
-    if (isAsciiNum(upper)) {
-      upperTranslated = upper - ZERO;
-    }
-    if (isHexDigit(upper)) {
-      upperTranslated = upper - A + 10;
-    }
-    if (upperTranslated === 1000) { // invalid seq
-      decoded[j++] = EQUALS;
-      decoded[j++] = upper;
-      decoded[j++] = lower;
-      continue;
-    }
-
-    if (isAsciiNum(lower)) {
-      lowerTranslated = lower - ZERO;
-    }
-    if (isHexDigit(lower)) {
-      lowerTranslated = lower - A + 10;
-    }
-    if (lowerTranslated === 1000) { // invalid seq
-      decoded[j++] = EQUALS;
+    const upperTranslated = hex[upper];
+    const lowerTranslated = hex[lower];
+    if ((upperTranslated | lowerTranslated) & 128) {
+      // invalid seq
       decoded[j++] = upper;
       decoded[j++] = lower;
       continue;
     }
     const shifted = upperTranslated << 4;
-    decoded[j++] = (shifted | lowerTranslated);
-  }
-  for (let i = runTo; i < len; i++) {
-    decoded[j++] = body[i];
+    decoded[j - 1] = shifted | lowerTranslated;
   }
   return decoded.slice(0, j);
 }
