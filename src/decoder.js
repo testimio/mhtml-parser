e3abd09536de0cb0077f9d7dfc4f05be140d2062
Benjamin Gruenbaum <inglor@gmail.com>
2018-12-19 10:46:05 +0200

    fix my mess

--- src/decoder.js
+++ src/decoder.js
@@ -1,3 +1,5 @@
+
+
 module.exports = (encoding, body) => {
   switch (encoding) {
     case 'base64':
@@ -16,8 +18,6 @@ module.exports = (encoding, body) => {
 const EQUALS = '='.charCodeAt(0);
 const CR = '\r'.charCodeAt(0);
 const LF = '\n'.charCodeAt(0);
-const ZERO = '0'.charCodeAt(0);
-const A = 'A'.charCodeAt(0);
 const D = 'D'.charCodeAt(0);
 const THREE = '3'.charCodeAt(0);
 
@@ -25,7 +25,7 @@ const translate = (() => {
   let str = 'switch (c) {\n'
   for (let i = 0; i <= 9; i++) str += `\tcase ${'0'.charCodeAt(0) + i}: return ${i};\n`;
   for (let i = 0xA; i <= 0xF; i++) str += `\tcase ${'A'.charCodeAt(0) + i - 0xA}: return ${i};\n`;
-  str += 'default: return 1000;\n';
+  str += 'default: return -1;\n'
   str += '};\n'
   return Function('c', str);
 })();
@@ -33,16 +33,14 @@ const translate = (() => {
 function convertQuotedPrintable(body) {
   const len = body.length;
   const decoded = Buffer.alloc(len); // at most this big
-  const runTo = len - 3;
   let j = 0;
+  const runTo = len - 3;
   for (let i = 0; i < runTo; i++) {
-    while ((decoded[j++] = body[i++]) !== EQUALS && i < runTo);
-    if (i >= runTo) {
-      break;
-    }
+    while (i < len && (decoded[j++] = body[i++]) !== EQUALS);
+    if (i >= runTo) break;
     // We are dealing with a '=xx' sequence.
-    const upper = body[++i] | 0;
-    const lower = body[++i] | 0;
+    const upper = body[i];
+    const lower = body[++i];
 
     // fast path for =3D
     if (upper === THREE && lower === D) {
@@ -52,25 +50,25 @@ function convertQuotedPrintable(body) {
       j--;
       continue;
     }
-    if (upper === LF) { // windows chrome does invalid encoding with \n and not \r\n
+    if (upper === LF) {
+      // windows chrome does invalid encoding with \n and not \r\n
       i--;
       j--;
       continue;
     }
-    let upperTranslated = translate(upper);
-    let lowerTranslated = translate(lower);
-    
-    if ((upperTranslated | lowerTranslated) & 128) { // invalid seq
+    const upperTranslated = translate(upper);
+    const lowerTranslated = translate(lower);
+    if ((upperTranslated | lowerTranslated) & 128) {
+      // invalid seq
       decoded[j++] = upper;
       decoded[j++] = lower;
       continue;
     }
-
     const shifted = upperTranslated << 4;
-    decoded[j - 1] = (shifted | lowerTranslated);
+    decoded[j - 1] = shifted | lowerTranslated;
   }
-  for (let i = runTo; i < len; i++) {
-    decoded[j++] = body[i];
+  for(let i = runTo; i < len; i++) {
+    decoded[j++] = body[i]
   }
   return decoded.slice(0, j);
 }
